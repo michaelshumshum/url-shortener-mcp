c17116b6c7f7d41f865cbc8d8f5dae67af579922
michaelshumshum <36499322+michaelshumshum@users.noreply.github.com>
2026-03-28 22:33:46 +0800

    fix: template strng release config

--- release.config.js
+++ release.config.js
@@ -11,8 +11,7 @@ export default {
             "@semantic-release/git",
             {
                 assets: ["CHANGELOG.md", "package.json"],
-                message:
-                    "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+                message: `chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}`,
             },
         ],
     ],
